4ed89bd6bba7e723745067433db95903b89394da
Raphael <raphael@fabco.tech>
2019-07-01 16:26:03 +0200

    - listen-data-at-name sends status 200 when the data-at-name is not found - json-schema validation for listen-data-at-name

--- src/listen-for-data-at-name.js
+++ src/listen-for-data-at-name.js
@@ -1,3 +1,4 @@
+const Ajv = require("ajv");
 const log = require("../utils").log;
 
 const parseEitherListeningNameData = require("../rchain")
@@ -5,14 +6,56 @@ const parseEitherListeningNameData = require("../rchain")
 const listenForDataAtName = require("../rchain").listenForDataAtName;
 const getValueFromBlocks = require("../rchain").getValueFromBlocks;
 
+const ajv = new Ajv();
+const schema = {
+  schemaId: "listen-data-at-name",
+  type: "object",
+  properties: {
+    name: {
+      type: "object",
+      properties: {
+        ids: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              id: {
+                type: "array",
+                items: { type: "number" }
+              }
+            },
+            required: ["id"]
+          }
+        }
+      },
+      required: ["ids"]
+    },
+    depth: { type: "number" }
+  },
+  required: ["name", "depth"]
+};
+ajv.addMetaSchema(require("ajv/lib/refs/json-schema-draft-06.json"));
+const validate = ajv.compile(schema);
+
 module.exports = function(req, res, rnodeClient) {
+  const valid = validate(req.body);
+  if (!valid) {
+    res
+      .status(400)
+      .json(validate.errors.map(e => `body${e.dataPath} ${e.message}`));
+    return;
+  }
+
   listenForDataAtName(req.body, rnodeClient)
     .then(either => {
       let blocks;
       try {
         blocks = parseEitherListeningNameData(either);
       } catch (err) {
-        res.status(400).json(err.message);
+        res.status(200).json({
+          success: false,
+          error: { message: err.message }
+        });
         return;
       }
 
@@ -20,15 +63,20 @@ module.exports = function(req, res, rnodeClient) {
       try {
         data = getValueFromBlocks(blocks);
         res.append("Content-Type", "text/plain; charset=UTF-8");
-        res.send(data);
+        res.send({
+          success: true,
+          data: data
+        });
       } catch (err) {
-        log("error: " + err);
-        res.status(404).json(err.message);
+        res.status(200).json({
+          success: false,
+          error: { message: err.message }
+        });
       }
     })
     .catch(err => {
       log("error : communication error with the node (GRPC endpoint)");
       log(err);
-      res.status(400).json(err.message);
+      res.status(500).json("Unable to communicate with the GRPC endpoint");
     });
 };
