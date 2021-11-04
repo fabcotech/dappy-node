66b87607fdad9e7fcc4a56db976b0c43f5db5d4e
Paul Musso <paul@musso.io>
2021-11-04 09:34:52 +0100

    add: get-contract-logs job

---
+++ src/jobs/get-contract-logs.js
@@ -0,0 +1,145 @@
+const rchainToolkit = require('rchain-toolkit');
+const { readLogsTerm, logs } = require('rchain-token');
+const redis = require('redis');
+
+function mandatory(varName, value) {
+  if (value === undefined) {
+    throw new Error(`${varName} env var is not defined`);
+  }
+  return value;
+}
+
+function parseBool(str) {
+  return (/^true$/i.test(str));
+} 
+
+function parseArray(contractStr) {
+  return (contractStr || '')
+    .split(',')
+    .filter((s) => !!s.length)
+    .map((s) => s.trim());
+}
+
+function parseRedisUrl(value) {
+  if (!/^redis:\/\/[a-zA-Z0-9._]+(:[0-9]{1,5})?\/[0-9]$/.test(value)) {
+    throw new Error(`${value} is not a redis url`);
+  }
+  return value;
+}
+
+function initConfig(env = {}){
+  return {
+    logsInteval: parseInt(env.LOGS_INTERVAL) || 10 * 1000,
+    masterRegistryUri: mandatory('MASTER_REGISTRY_URI', env.MASTER_REGISTRY_URI),
+    contracts: parseArray(mandatory('CONTRACTS', env.CONTRACTS)),
+    rnodeUri: mandatory('RNODE_URI', env.RNODE_URI),
+    redisUrl: parseRedisUrl(mandatory('REDIS_URL', env.REDIS_URL)),
+  }
+} 
+
+function initClientRedis(redisUrl) {
+  return redis.createClient(redisUrl);
+}
+
+function getZAdd(client) {
+  return args => new Promise((resolve, reject) => {
+    client.zadd(args, (errors, results) => {
+      if (errors) reject(errors);
+      else resolve(results);
+    });
+  });
+}  
+
+function parseLogTs(l) {
+  return parseInt(l.match(/^[^,]+,(\d+),/)[1]);
+}
+
+function parseLogs(rawLogs) {
+  return rawLogs
+    .split(';')
+    .filter(l => l.length)
+    .filter(l => {
+      try {
+        logs.checkLine(l);
+        return true;
+      } catch (err) {
+        console.log(l);
+        console.error(err);
+        return false; 
+      }
+    })
+    .map(l => ({
+      ts: parseLogTs(l),
+      msg: logs.formatLine(l).trim()
+    }));
+}
+
+async function queryLogs(exploreDeploy, masterRegistryUri, rnodeUri, contractId) {
+  const result = await exploreDeploy(
+    rnodeUri,
+    {
+      term: readLogsTerm({
+        masterRegistryUri: masterRegistryUri,
+        contractId: contractId,
+      })
+    }
+  );
+
+  return parseLogs(rchainToolkit.utils.rhoValToJs(
+    JSON.parse(result).expr[0]
+  ));
+}
+
+async function saveToSortedSetsInRedis(zAdd, contract, logs) {
+  if (!logs.length) return;
+
+  const nbEntries = await zAdd([
+    contract, 
+    ...logs.map(l => [l.ts, l.msg]).flat()
+  ]);
+
+  if (nbEntries) {
+    console.info(`Added ${nbEntries} entries`);
+  }
+}
+
+async function saveContractLogs(exploreDeploy, zAdd, { masterRegistryUri, rnodeUri, contracts }) {
+  for (let contract of contracts) {
+    try {
+      const logs = await queryLogs(exploreDeploy, masterRegistryUri, rnodeUri, contract)
+      await saveToSortedSetsInRedis(zAdd, contract, logs);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+function waitDuration(milliseconds) {
+  return new Promise((res) => setTimeout(() => res(), milliseconds));
+}
+
+async function startJob() {
+  const config = initConfig(process.env);
+  const redisClient = initClientRedis(config.redisUrl);
+  const zAdd = getZAdd(redisClient);
+
+  console.log('get contract logs started');
+
+  while (true) {
+    await saveContractLogs(rchainToolkit.http.exploreDeploy, zAdd, config);
+    await waitDuration(config.logsInteval);
+  }  
+}
+
+parseBool(process.env.START_JOB) && startJob();
+
+module.exports = {
+  parseArray,
+  parseBool,
+  parseRedisUrl,
+  saveContractLogs,
+  saveToSortedSetsInRedis,
+  queryLogs,
+  initConfig,
+};
---
+++ src/jobs/get-contract-logs.spec.js
@@ -0,0 +1,102 @@
+var chai = require('chai');
+var spies = require('chai-spies');
+
+var expect = chai.expect;
+chai.use(spies);
+
+const {
+  parseArray,
+  parseRedisUrl,
+  saveContractLogs,
+  saveToSortedSetsInRedis,
+  parseBool,
+  initConfig,
+  queryLogs,
+} = require('./get-contract-logs');
+
+const createPurchaseLog = (date, toBox, fromBox, amount, price, fromToken, token) =>
+  `p,${date.getTime()},${toBox},${fromBox},${amount},${price},${fromToken},${token}`;
+
+describe('get-contract-logs', () => {
+  it('should parse contract name', () => {
+    expect(
+      parseArray("FOO ,BAR, BAZ FOO ")
+    ).to.eql(['FOO','BAR','BAZ FOO']);
+    expect(parseArray(undefined)).to.eql([]);
+    expect(parseArray(",foo,,")).to.eql(['foo']);
+  });
+  it('should parseBool', () => {
+    expect(parseBool(undefined)).to.eql(false);
+    expect(parseBool("True")).to.be.true;
+    expect(parseBool("FalsE")).to.be.false;
+  });
+  it('should parse parseRedisUrl', () => {
+    expect(() => parseRedisUrl('redis://1.2.3.4/1')).to.not.throw();
+    expect(() => parseRedisUrl('redis://1.2.3.4:1234/1')).to.not.throw();
+    expect(() => parseRedisUrl('redis://bar/1')).to.not.throw();
+    expect(() => parseRedisUrl('redis://bar_baz.dev/1')).to.not.throw();
+    expect(() => parseRedisUrl('redis://bar_baz:1454/1')).to.not.throw();
+  });
+  it('should check default config', () => {
+    expect(initConfig).to.throw('MASTER_REGISTRY_URI env var is not defined');
+
+    const config = initConfig({
+      MASTER_REGISTRY_URI: 'foo',
+      RNODE_URI: 'http://foo',
+      CONTRACTS: 'foo,bar',
+      REDIS_URL: 'redis://redis_host:1234/4'
+    });
+    expect(config.logsInteval).to.eql(10 * 1000);
+    expect(config.contracts).to.eql(['foo','bar']);
+  });
+  it('should queryLogs', async () => {
+    const exploreDeploy = () => Promise.resolve(`{
+      "expr": [
+        { "ExprString": { "data": "${
+          createPurchaseLog(new Date('2021-11-02T14:59+00:00'),'aaa','aaa',1,50000000,0,'foo')
+        };${
+          createPurchaseLog(new Date('2021-11-02T15:05+00:00'),'aaa','aaa',1,10000,0,'bar')
+        }" }}
+      ]
+    }`);
+    // const exploreDeploy = require('rchain-toolkit').http.exploreDeploy;
+    const logs = await queryLogs(exploreDeploy,
+      'pridt69gz6tnsux888afui7wegoq4fcywoze5bm6rt3cbnne5kn1sk',
+      'http://rnode.http.dev',
+      'dappynamesystem'
+    );
+
+    expect(logs).to.eql([
+      { ts: 1635865140000, msg: "2021-11-02T14:59 box aaa purchased NFT foo from box aaa at price 50000000" },
+      { ts: 1635865500000, msg: "2021-11-02T15:05 box aaa purchased NFT bar from box aaa at price 10000" }
+    ]);
+  });
+
+  it('should queryLogs empty', async () => {
+    const logs = await queryLogs(() => Promise.resolve(`{
+      "expr": [
+        { "ExprString": { "data": "" }}
+      ]
+    }`), 'foo', 'http://rnode.http.dev', 'dappynamesystem');
+
+    expect(logs).to.eql([]);
+  });
+
+  it('should save to sorted sets', async () => {
+    const zAdd = chai.spy();
+    const logs = [
+      { ts: 1635956492287, msg: "msg 1" },
+      { ts: 1635952883276 , msg: "msg 2" },
+    ];
+    await saveToSortedSetsInRedis(zAdd, 'foo', logs);
+
+    expect(zAdd).have.been.called.with(['foo', logs[0].ts, logs[0].msg, logs[1].ts, logs[1].msg]);
+  });
+  it('should save nothing if no logs', async () => {
+    const zAdd = chai.spy();
+    const logs = [];
+    await saveToSortedSetsInRedis(zAdd, 'foo', logs);
+
+    expect(zAdd).not.have.been.called();
+  });
+});
