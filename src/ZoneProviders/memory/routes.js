971aa7e0e5110f93ca9bd0a8d03036f1b890efa5
Paul Musso <paul@musso.io>
2022-03-23 15:29:28 +0100

    chore: remove unused route

--- src/ZoneProviders/memory/routes.js
+++ src/ZoneProviders/memory/routes.js
@@ -1,15 +1,8 @@
 const { Router } = require('express');
-const bodyParser = require('body-parser');
-
-const { dnsQuery } = require('./dns-query');
 
 module.exports = {
   getRoutes: () => {
     const router = Router();
-
-    router.post('/dns-query', bodyParser.raw({
-      type: 'application/dns-message',
-    }), dnsQuery);
 
     return router;
   },
