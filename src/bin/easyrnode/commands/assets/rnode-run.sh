5888a20e4153030f1aae8ef65964bbb195ab1cc5
Paul Musso <paul@musso.io>
2022-05-13 15:31:47 +0200

    fix: missing shardId in dappy-deploy-name-system

--- src/bin/easyrnode/commands/assets/rnode-run.sh
+++ src/bin/easyrnode/commands/assets/rnode-run.sh
@@ -3,7 +3,7 @@
 cat << EOF | docker-compose -f /dev/stdin up
 services:
   rnode:
-    image: rchain/rnode:latest
+    image: rchain/rnode:v0.13.0-alpha3
     volumes:
     - $PWD/.rnode:/var/lib/rnode
     ports:
@@ -11,7 +11,7 @@ services:
     - 40402:40402
     command: run -s --validator-private-key ${RNODE_PRIVATE_KEY:-28a5c9ac133b4449ca38e9bdf7cacdce31079ef6b3ac2f0a080af83ecff98b36} --dev-mode --shard-name dev
   rnode-propose:
-    image: rchain/rnode:latest
+    image: rchain/rnode:v0.13.0-alpha3
     links:
     - rnode
     entrypoint: ''
