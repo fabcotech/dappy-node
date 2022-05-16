c4b5e96f7aad0c450fc4fc583f24c6e9b1d46d6a
Paul Musso <paul@musso.io>
2022-05-16 19:03:48 +0200

    fix: add dappynode to altname certificate

--- src/server/index.ts
+++ src/server/index.ts
@@ -41,7 +41,7 @@ const getOrCreateCertificate = (
         days: SELF_SIGNED_CERTIFICATE_DURATION,
         selfSigned: true,
         commonName: 'dappynode',
-        altNames: ['localhost'],
+        altNames: ['localhost', 'dappynode'],
       },
       (err, certificate) => {
         if (err) {
